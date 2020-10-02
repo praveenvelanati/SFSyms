7ceb791fc42905c39d50ee4e4510533485e34d3e
Praveen <praveen.velanati@sparity.com>
2020-10-02 01:29:20 -0400

    added more targets

--- Package.swift
+++ Package.swift
@@ -9,9 +9,9 @@ let package = Package(
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "Lib1",
-            targets: ["Tar1"]),
+            targets: ["Tar1A", "Tar1B"]),
         .library(name: "Lib2",
-                 targets: ["Tar2"])
+                 targets: ["Tar2A", "Tar2B"])
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -21,10 +21,16 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "Tar1",
+            name: "Tar1A",
             dependencies: []),
         .target(
-            name: "Tar2",
+            name: "Tar1B",
+            dependencies: []),
+        .target(
+            name: "Tar2A",
+            dependencies: []),
+        .target(
+            name: "Tar2B",
             dependencies: [])
     ]
 )
