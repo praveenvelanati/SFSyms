354ed077fa4be79a1c97b9557fc7b3befb545292
Praveen <praveen.velanati@sparity.com>
2020-10-02 02:45:56 -0400

    Tesying

--- Package.swift
+++ Package.swift
@@ -20,7 +20,8 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-        .target(name: "Tar1A"),
+        .target(name: "Tar1A",
+                dependencies: ["Package1"]),
         .target(name: "Tar1B"),
         .target(name: "Tar2A"),
         .target(name: "Tar2B")
