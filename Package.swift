6e09b5c4360b5be4015af3bd4cfe78525cf8dc0e
Praveen <praveen.velanati@sparity.com>
2020-10-02 02:59:14 -0400

    Tesyinghh

--- Package.swift
+++ Package.swift
@@ -21,9 +21,9 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(name: "Tar1A",
-                dependencies: ["Lib1FromPk1"]),
+                dependencies: ["Package1"]),
         .target(name: "Tar1B",
-                dependencies: ["Lib2FromPk1"]),
+                dependencies: ["Package1"]),
         .target(name: "Tar2A"),
         .target(name: "Tar2B")
     ]
--- Package1/Package.swift
+++ Package1/Package.swift
@@ -8,10 +8,8 @@ let package = Package(
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
-            name: "Lib1FromPk1",
-            targets: ["Pkg1Tar1"]),
-        .library(name: "Lib2FromPk1",
-                 targets: ["Pkg1Tar2"])
+            name: "Package1",
+            targets: ["Pkg1Tar1", "Pkg1Tar2"])
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
