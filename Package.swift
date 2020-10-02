3e5b09777a2f521fa2c233c93469b1da038c5231
Praveen <praveen.velanati@sparity.com>
2020-10-02 00:59:57 -0400

    Updated package name

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
-            name: "PkSymbols",
+            name: "Symbs",
             targets: ["PkSymbols", "TargA"]),
     ],
     dependencies: [
