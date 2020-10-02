ca1d252343cf51947638f406531866c52e67b26f
Praveen <praveen.velanati@sparity.com>
2020-10-02 02:40:29 -0400

    fixed nesting issue

--- Package1/Package.swift
+++ Package1/Package.swift
@@ -1,6 +1,6 @@
 // swift-tools-version:5.2
 // The swift-tools-version declares the minimum version of Swift required to build this package.
-
+// Nested
 import PackageDescription
 
 let package = Package(
@@ -15,7 +15,7 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(path: "Package1")
+       // .package(path: "Package1")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
